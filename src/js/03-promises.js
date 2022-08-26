171b5d17557eca66a6a2b31b3d4dc9e53371e2f1
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-08-26 23:03:21 +0200

    updated task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,11 @@
+const refs = {
+  inputDelayEl: document.querySelector('[name="delay"]'),
+  inputStepEl: document.querySelector('[name="step"]'),
+  inputAmountEl: document.querySelector('[name="amout"]'),
+  submitButtonEl: document.querySelector('button'),
+  form: document.querySelector('form'),
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
