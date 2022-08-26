f95e64cc1112b131593047eb15be97b5d14bb2bc
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-08-26 21:03:46 +0000

    Deploying to gh-pages from @ Mariana-Diachun/goit-js-hw-09@171b5d17557eca66a6a2b31b3d4dc9e53371e2f1 🚀

---
+++ 03-promises.eb26cbc0.js
@@ -0,0 +1,2 @@
+document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amout"]'),document.querySelector("button"),document.querySelector("form");
+//# sourceMappingURL=03-promises.eb26cbc0.js.map
