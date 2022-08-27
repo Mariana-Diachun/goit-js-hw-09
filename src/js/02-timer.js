ad9e82811f0e181c977478d88789d7a89c02f454
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-08-27 17:32:19 +0200

    added changes to task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,18 +6,39 @@ const options = {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
+  // onClose(selectedDates) {
+  //   console.log(selectedDates[0]);
+  // },
 };
 
 const refs = {
     inputEl: document.querySelector('#datetime-picker'),
     buttonEl: document.querySelector('button[data-start]')
 }
+refs.buttonEl.disabled = true;
 
 const fp = flatpickr(refs.inputEl, {
-    enableTime: true,
-    dateFormat: "Y-m-d H:i",
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const choosedTime = selectedDates[0].getTime();
+    const timeNow = options.defaultDate.getTime();
+    if (choosedTime < timeNow) {
+      return window.alert("Please choose a date in the future");
+    } else {
+      refs.buttonEl.disabled = false;
+    }
+  },
 });
-console.log(fp);
+
+refs.buttonEl.addEventListener('click', onButtonStartClick);
+function onButtonStartClick(evt) {
+    setInterval(() => {
+      const currentTime = Date.now();
+      console.log(currentTime);
+      // console.log(choosedTime - currentTime);
+      console.log(options.onClose());
+      }, 1000);
+}
