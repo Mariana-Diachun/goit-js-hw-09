dbcf4272b4e58777e0b81b93718003ed59259362
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-08-29 21:41:11 +0200

    added final js code to task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,46 +3,41 @@ import Notiflix from 'notiflix';
 const refs = {
   inputDelayEl: document.querySelector('[name="delay"]'),
   inputStepEl: document.querySelector('[name="step"]'),
-  inputAmountEl: document.querySelector('[name="amout"]'),
-  submitButtonEl: document.querySelector('button'),
+  inputAmountEl: document.querySelector('[name="amount"]'),
   form: document.querySelector('form'),
 };
 
-let firstDelayEl = null;
-let delayStepEl = null;
-let amountEl = null;
-
 refs.form.addEventListener('submit', onFormSubmit);
-
-
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
-        resolve({ position, delay })
+        resolve({ position, delay });
       } else {
-        reject({ position, delay })
+        reject({ position, delay });
       }
-    }, delay)
+    }, delay);
   })
 }
 
   function onFormSubmit(evt) {
     evt.preventDefault();
-    let position = Number(refs.inputDelayEl.value);
-    let delay = Number(refs.inputStepEl.value);
+
+    let delay = Number(refs.inputDelayEl.value);
+    let delayStep = Number(refs.inputStepEl.value);
     let promiseValue = Number(refs.inputAmountEl.value);
 
     for (let i = 0; i < promiseValue; i += 1) {
-      createPromise()
+      createPromise(i + 1, delay)
   .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    Notiflix.Notify.failure(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   });
+      delay += delayStep;
     }
-    
 };
+
